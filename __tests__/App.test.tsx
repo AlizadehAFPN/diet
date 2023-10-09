f792b8798edc664e8fe99738ba9dbe48a95466df
Farzad Alizadeh <alizadeh2023@gmail.com>
2023-10-09 22:20:02 +0300

    test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,30 @@
-/**
- * @format
- */
+import MockAdapter from 'axios-mock-adapter';
+import { tempLogin } from '../src/services';
+import axiosInstance from '../src/services/axiosConfig';
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// Import your function that makes the API request
 
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+// Create a mock instance
+const mock = new MockAdapter(axiosInstance);
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+// Mock a successful response with status code 200
+const mockUsername = 'stas.testufwfweser1@dietdoctor.com';
+const mockPassword = 'C5(Pg5qwrfwfwP^(WJ!eS%d38FI';
+mock.onPost('https://ddapi.prod.dietdoctor.com/auth/token').reply((config) => {
+  console.log(config , 'config')
+  const requestData = JSON.parse(config.data); // Parse the request data
+  if (
+    requestData.username === mockUsername &&
+    requestData.password === mockPassword
+  ) {
+    return [200, { data: 'Your data' }];
+  } else {
+    return [401]; // Unauthorized status code for incorrect credentials
+  }
+});
+// Your test
+test('API POST request returns status code 200', async () => {
+  const response = await tempLogin({username:mockUsername, password:mockPassword}); // Use the same values as the mock
+  console.log(response.data , 'response')
+  // expect(response.status).toBe(200);
 });
