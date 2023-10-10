92e16521ca36c032e6a53c252c06d69b8c3a4a9c
yousof <dolphin@dolphins-Mac-mini.local>
2023-10-11 00:04:56 +0330

    unused files

--- android/app/src/main/java/com/dietdoctor/MyTextInputModule.java
+++ android/app/src/main/java/com/dietdoctor/MyTextInputModule.java
@@ -1,20 +1,20 @@
-// package com.dietdoctor;
+package com.dietdoctor;
 
-// import android.view.View;
-// import com.facebook.react.uimanager.SimpleViewManager;
-// import com.facebook.react.uimanager.ThemedReactContext;
+import android.view.View;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
 
-// public class MyTextInputModuleManager extends SimpleViewManager<View> {
-//     public static final String REACT_CLASS = "MyTextInput";
+public class MyTextInputModuleManager extends SimpleViewManager<View> {
+    public static final String REACT_CLASS = "MyTextInput";
 
-//     @Override
-//     public String getName() {
-//         return REACT_CLASS;
-//     }
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
 
-//     @Override
-//     protected View createViewInstance(ThemedReactContext reactContext) {
-//         // Create and return your custom text input view here
-//         return new MyTextInputView(reactContext);
-//     }
-// }
+    @Override
+    protected View createViewInstance(ThemedReactContext reactContext) {
+        // Create and return your custom text input view here
+        return new MyTextInputView(reactContext);
+    }
+}
--- android/app/src/main/java/com/dietdoctor/MyTextInputView.java
+++ android/app/src/main/java/com/dietdoctor/MyTextInputView.java
@@ -1,63 +1,63 @@
-// package com.dietdoctor;
+package com.dietdoctor;
 
-// import android.content.Context;
-// import android.graphics.Color;
-// import android.graphics.Typeface;
-// import android.text.InputType;
-// import android.util.TypedValue;
-// import android.view.Gravity;
-// import android.view.ViewGroup.LayoutParams;
-// import android.widget.EditText;
-// import android.widget.LinearLayout;
-// import android.widget.TextView;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.InputType;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-// import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ThemedReactContext;
 
-// public class MyTextInputView extends LinearLayout {
-//     private EditText editText;
+public class MyTextInputView extends LinearLayout {
+    private EditText editText;
 
-//     public MyTextInputView(ThemedReactContext context) {
-//         super(context);
+    public MyTextInputView(ThemedReactContext context) {
+        super(context);
 
-//         // Create a vertical LinearLayout to hold the EditText and TextView
-//         setOrientation(LinearLayout.VERTICAL);
-//         setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        // Create a vertical LinearLayout to hold the EditText and TextView
+        setOrientation(LinearLayout.VERTICAL);
+        setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         
-//         // Create and customize the EditText
-//         editText = new EditText(context);
-//         editText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-//         editText.setHint("Enter text..."); // Hint text
-//         editText.setTextColor(Color.BLACK); // Text color
-//         editText.setBackgroundColor(Color.WHITE); // Background color
-//         editText.setInputType(InputType.TYPE_CLASS_TEXT); // Text input type
-//         editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16); // Text size
-//         editText.setPadding(16, 16, 16, 16); // Padding
+        // Create and customize the EditText
+        editText = new EditText(context);
+        editText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        editText.setHint("Enter text..."); // Hint text
+        editText.setTextColor(Color.BLACK); // Text color
+        editText.setBackgroundColor(Color.WHITE); // Background color
+        editText.setInputType(InputType.TYPE_CLASS_TEXT); // Text input type
+        editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16); // Text size
+        editText.setPadding(16, 16, 16, 16); // Padding
 
-//         // Add the EditText to the LinearLayout
-//         addView(editText);
+        // Add the EditText to the LinearLayout
+        addView(editText);
 
-//         // Create and customize a TextView (optional)
-//         TextView textView = new TextView(context);
-//         textView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-//         textView.setText("Optional description"); // Optional description
-//         textView.setTextColor(Color.GRAY); // Text color
-//         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12); // Text size
-//         textView.setTypeface(null, Typeface.ITALIC); // Text style
-//         textView.setGravity(Gravity.END); // Text alignment
-//         textView.setPadding(16, 0, 16, 8); // Padding
+        // Create and customize a TextView (optional)
+        TextView textView = new TextView(context);
+        textView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        textView.setText("Optional description"); // Optional description
+        textView.setTextColor(Color.GRAY); // Text color
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12); // Text size
+        textView.setTypeface(null, Typeface.ITALIC); // Text style
+        textView.setGravity(Gravity.END); // Text alignment
+        textView.setPadding(16, 0, 16, 8); // Padding
 
-//         // Add the TextView to the LinearLayout (optional)
-//         addView(textView);
-//     }
+        // Add the TextView to the LinearLayout (optional)
+        addView(textView);
+    }
 
-//     // Add additional methods and properties here as needed
-//     // For example, you can create methods to get or set the text value of the EditText.
+    // Add additional methods and properties here as needed
+    // For example, you can create methods to get or set the text value of the EditText.
 
-//     public String getText() {
-//         return editText.getText().toString();
-//     }
+    public String getText() {
+        return editText.getText().toString();
+    }
 
-//     public void setText(String text) {
-//         editText.setText(text);
-//     }
-// }
+    public void setText(String text) {
+        editText.setText(text);
+    }
+}
